adc66eff55e3bc3b7d3fcac7adba58749e0a6517
Evan Furniss <evanfurniss@gmail.com>
2020-12-31 13:00:05 -0600

    Created a list of options for password that appear after 'Generate Password' is clicked and hides the generate button. Also created arrays identifying all letters, and special characters.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# password-generator
+# Password Generator
+
+## Overview
+
+## Resources
+
+## Repository Links
+
+https://github.com/evanfurniss/password-generator
+
+evanfurniss.github.io/password-generator/
+
+## Images
--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordOpt = document.querySelector("#passOpt");
+var specialChar = ["!", "?", "@", "$", "&", "*"];
+var letterArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Write password to the #password input
 function writePassword() {
+  generateBtn.style.display = "none";
+  passwordOpt.style.display = "inline-block";
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
