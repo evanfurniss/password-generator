fc913d1176833646c05f6b7130e59d36f8ab0540
Evan Furniss <evanfurniss@gmail.com>
2020-12-31 17:27:51 -0600

    Created function to create a random password based on the criteria. Removed the create button as it was causing confusion.

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var createBtn = document.querySelector("#createBtn");
-var passwordOpt = document.querySelector("#passOpt");
-var passLength = document.querySelector("#passLength").value;
 
-var specialChar = ["!", "?", "@", "$", "%", "&", "*"];
-var letterArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-var checkNumber = document.querySelector("#includeNum");
-var checkLwrLetter = document.querySelector("lwrCase");
-var checkUprLetter = document.querySelector("uprCase");
-var checkSpeChar = document.querySelector("speChars");
+var numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var letterLwrArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var letterUprArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialCharArr = ["!", "?", "@", "$", "%", "&", "*"];
 
 // Write password to the #password input
 function writePassword() {
-  generateBtn.style.display = "none";
-  passwordOpt.style.display = "inline-block";
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   function generatePassword() {
-    
+    var pushPassword = "";
+    var characterArr = [];
+
+    if (document.querySelector("#includeNum").checked) {
+      characterArr = characterArr.concat(numArr);
+    }
+    if (document.querySelector("#lwrCase").checked) {
+      characterArr = characterArr.concat(letterLwrArr);
+    }
+    if (document.querySelector("#uprCase").checked) {
+      characterArr = characterArr.concat(letterUprArr);
+    }
+    if (document.querySelector("#speChars").checked) {
+      characterArr = characterArr.concat(specialCharArr);
+    }
+    for (let i = 0; i < document.querySelector("#passLength").value; i++) {
+      var ranArrSelector = Math.floor(Math.random() * characterArr.length);
+
+      pushPassword += characterArr[ranArrSelector];
+    }
+    return pushPassword;
   }
 
   passwordText.value = password;
-
-  createBtn.addEventListener("click", generatePassword);
 }
 
 // Add event listener to generate button
