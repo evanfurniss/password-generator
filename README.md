f292bab3d669bbcdc285cd3f1eb12e0dd545d8bd
Evan Furniss <evanfurniss@gmail.com>
2021-01-05 10:43:01 -0600

    Updated project to operate based on the business side's specifications. Updated images, commented out HTML form and js that checked the form, modified function to ask parameters using prompts/confirms.

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## Overview
 
-This project creates a randomly generated password of the users choosing. Users can select the requirements for the password including length, numbers, lower case letters, upper case letters, and special characters. Once the users's selected length and characters are filled, users will click the "Generate Password" button, which will generate their random password and display it in the text area. 
+This project creates a randomly generated password of the users choosing. Users can select the requirements for the password including length, numbers, lower case letters, upper case letters, and special characters by answering prompts/confirms to choose parameters. Users will click the "Generate Password" button, which gives the user several prompts/confirms listed above, which will generate their random password using those answers, and display it in the text area. 
 
-The password generator uses JavaScript as the primary language to create and run functions that will take users' requirements and modify the elements on the HTML and CSS files. The password is created using the random math functions, for loops, if/else statements, dev created functions, and runs using eventListeners.
+The password generator uses JavaScript as the primary language to create and run functions that will take users' requirements and modify the elements on the HTML file. The password is created using the random math functions, for loops, prompts/confirms, if/else statements, dev created functions, and runs using eventListeners.
 
 Users can utilize this tool to quickly create secure passwords while creating accounts, updating old passwords, or adding a level of security they may not have had otherwise.
 
@@ -22,6 +22,8 @@ https://evanfurniss.github.io/password-generator/
 
 <img src="pics/base.png">
 <br>
-<img src="pics/min-success.png">
+<img src="pics/prompt.png">
 <br>
-<img src="pics/max-success.png">
+<img src="pics/min-value.png">
+<br>
+<img src="pics/max-value.png">
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 var createBtn = document.querySelector("#createBtn");
 
@@ -7,7 +6,6 @@ var letterLwrArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
 var letterUprArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var specialCharArr = ["!", "?", "@", "$", "%", "&", "*"];
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -15,20 +13,28 @@ function writePassword() {
   function generatePassword() {
     var pushPassword = "";
     var characterArr = [];
+    var passLength = prompt("Choose a password length between 8-128");
 
-    if (document.querySelector("#includeNum").checked) {
+    if (passLength < 8 || passLength > 128) {
+      passLength = prompt("Value out of range. Choose a number between 8-128");
+    }
+    else if (isNaN(passLength)) {
+      passLength = prompt("Value is not a number. Choose a number between 8-128.");
+    }
+
+    if (confirm("Click OK to include numbers")) {
       characterArr = characterArr.concat(numArr);
     }
-    if (document.querySelector("#lwrCase").checked) {
+    if (confirm("Click OK to include lower case letters")) {
       characterArr = characterArr.concat(letterLwrArr);
     }
-    if (document.querySelector("#uprCase").checked) {
+    if (confirm("Click OK to include upper case letters")) {
       characterArr = characterArr.concat(letterUprArr);
     }
-    if (document.querySelector("#speChars").checked) {
+    if (confirm("Click OK to include special characters")) {
       characterArr = characterArr.concat(specialCharArr);
     }
-    for (let i = 0; i < document.querySelector("#passLength").value; i++) {
+    for (let i = 0; i < passLength; i++) {
       var ranArrSelector = Math.floor(Math.random() * characterArr.length);
 
       pushPassword += characterArr[ranArrSelector];
@@ -36,8 +42,30 @@ function writePassword() {
     return pushPassword;
   }
 
+  // NOTE: Uncomment this and the div tag of the HTML to use check boxes instead of prompts.
+  //       Below is a usable code to use checkboxes on the screen.  
+  //
+  //   if (document.querySelector("#includeNum").checked) {
+  //     characterArr = characterArr.concat(numArr);
+  //   }
+  //   if (document.querySelector("#lwrCase").checked) {
+  //     characterArr = characterArr.concat(letterLwrArr);
+  //   }
+  //   if (document.querySelector("#uprCase").checked) {
+  //     characterArr = characterArr.concat(letterUprArr);
+  //   }
+  //   if (document.querySelector("#speChars").checked) {
+  //     characterArr = characterArr.concat(specialCharArr);
+  //   }
+  //   for (let i = 0; i < document.querySelector("#passLength").value; i++) {
+  //     var ranArrSelector = Math.floor(Math.random() * characterArr.length);
+  //
+  //     pushPassword += characterArr[ranArrSelector];
+  //   }
+  //   return pushPassword;
+  // }
+
   passwordText.value = password;
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
